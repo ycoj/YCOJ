6d7a8e4db95dddbf15706a01c12250a5059954cf
Tairui Qu <XiaoQuQu_CN@outlook.com>
2026-01-17 22:31:38 +0800

    feat(hydrooj): add languages handler

--- packages/hydrooj/src/handler/ui.ts
+++ packages/hydrooj/src/handler/ui.ts
@@ -1,7 +1,10 @@
 import { PERM, PRIV } from '@hydrooj/common';
 import { Context } from '../context';
+import { NotFoundError } from '../error';
+import ProblemModel from '../model/problem';
+import { langs } from '../model/setting';
 import user from '../model/user';
-import { Handler } from '../service/server';
+import { Handler, param, Types } from '../service/server';
 
 export class NavHandler extends Handler {
     async get({ domainId }) {
@@ -11,6 +14,51 @@ export class NavHandler extends Handler {
     }
 }
 
+export class AvailableLanguageHandler extends Handler {
+    @param('pid', Types.Int)
+    async get(domainId: string, pid: number) {
+        interface AvailableLanguageResponse {
+            languages: Record<string, {
+                display: string;
+                versions: {
+                    display: string;
+                    name: string;
+                }[];
+            }>;
+        }
+
+        const pdoc = await ProblemModel.get(domainId, pid);
+        if (!pdoc) throw new NotFoundError(pid);
+        const languages: AvailableLanguageResponse['languages'] = {};
+
+        for (const lang of Object.keys(langs)) {
+            if (langs[lang].disabled || langs[lang].hidden) continue;
+            if (lang.includes('.')) continue; // 先处理 Language Family
+            languages[lang] = {
+                display: langs[lang].display,
+                versions: [
+                    {
+                        display: langs[lang].display,
+                        name: lang,
+                    },
+                ],
+            };
+        }
+
+        for (const lang of Object.keys(langs)) {
+            if (langs[lang].disabled || langs[lang].hidden) continue;
+            if (!lang.includes('.')) continue; // 处理 Language Version
+            languages[lang.split('.')[0]].versions.push({
+                display: langs[lang].display,
+                name: lang,
+            });
+        }
+
+        this.response.body = { languages };
+    }
+}
+
 export async function apply(ctx: Context) {
     ctx.Route('ui_nav', '/ui/nav', NavHandler);
+    ctx.Route('ui_languages', '/ui/languages', AvailableLanguageHandler);
 }
